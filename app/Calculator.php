1422a65ef321bcc34dd271580aa614401a9a986a
Chris Rayner <crayner@spartaglobal.com>
2024-10-08 14:38:00 +0100

    feat: add input handling and calculation methods to Calculator class
    - Added new properties: `$numberCalculated` and `$operator` to manage the state of the calculator.
    - Introduced `inputNumber()`, `selectOperator()`, and `calculate()` methods to handle user inputs and perform calculations.
    - Modified arithmetic methods (`add`, `subtract`, `multiply`, `divide`) to be private and return float values.
    - Added `clear()` method to reset the calculator state.

--- app/Calculator.php
+++ app/Calculator.php
@@ -6,31 +6,78 @@ class Calculator{
 
     private $numberOne;
     private $numberTwo;
+    private $numberCalculated;
+    private $operator;
 
 
-    public function add($x, $y) : int
+    private function add($x, $y) : float
     {
         return $x + $y;
     }
 
-    public function subtract($x, $y) : int 
+    private function subtract($x, $y) : float
     {
         return $x - $y;
     }
 
-    public function multiply($x, $y) : int
+    private function multiply($x, $y) : float
     {
         return $x * $y;
     }
 
-    public function divide($x, $y) : float
+    private function divide($x, $y) : float
     {
-        if($y === 0){
+        if($y === 0)
+        {
             echo "ARITHMETIC ERR: Cannot divide by 0!";
             return 0;
         }
-        else{
+        else
+        {
             return $x / $y;
         }
+    }
+
+    public function inputNumber($numberInput){
+        if($this->operator === "initial")
+        {
+            $this->numberOne = $numberInput;
+        }
+        else
+        {
+            $this->numberTwo = $numberInput;
+        }
+    }
+
+    public function selectOperator($operator)
+    {
+        switch($operator)
+        {
+            case "addition" : $this->operator = "addition";
+            case "subtraction": $this->operator = "subtraction";
+            case "multiplication": $this->operator = "multiplication";
+            case "division" : $this->operator = "division";
+        }
+    }
+
+    public function calculate()
+    {
+        switch($operator)
+        {
+            case "addition": $this->numberCalculated = divide();
+            case "subtraction": $this->numberCalculated = subtract();
+            case "multiplication": $this->numberCalculated = multiply();
+            case "division": $this->numberCalculated = divide();
+        }
+        $this->numberOne = $numberCalculated;
+        $this->numberTwo = 0;
+        $this->operator = "initial";
+        return $numberCalculated;
+    }
+
+    public function clear(){
+        $this->numberOne = 0;
+        $this->numberTwo = 0;
+        $this->operator = "initial";
     }
 }
